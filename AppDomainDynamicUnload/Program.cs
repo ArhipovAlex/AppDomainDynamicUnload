7dc179fa8164dfda885ed29f42c418bc8426264e
Sania <Sania@DESKTOP-3HL58UN>
2024-05-18 21:30:23 +0800

    Domains in process

--- AppDomainDynamicUnload/Program.cs
+++ AppDomainDynamicUnload/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
 
 namespace AppDomainDynamicUnload
 {
@@ -10,6 +12,20 @@ namespace AppDomainDynamicUnload
 	{
 		static void Main(string[] args)
 		{
+			//1) Создаем домен приложения:
+			AppDomain domain=AppDomain.CreateDomain("ConsoleDomain");
+			//2) Загружаем DLL в созданный домен:
+			Assembly asm = domain.Load(AssemblyName.GetAssemblyName("SampleLibrary.dll"));
+			//3) Получаем модуль из которого будем выполнять вызов:
+			Module module = asm.GetModule("SampleLibrary.dll");
+			//4) Получаем класс из DLL-библиотеки:
+			Type type = module.GetType("SampleLibrary.SampleClass");
+			//5) Вытаскиваем метод из класса, который будем вызывать:
+			MethodInfo method = type.GetMethod("Hello");
+			//6) Вызываем метод:
+			method.Invoke(null, null);
+
+			AppDomain.Unload(domain);
 		}
 	}
 }
